9b9edccec1a74e5ff734ffe84ca0b2379f15be84
Owen Brasier <owen.brasier@sydney.edu.au>
2019-01-19 17:33:01 +1100

    added archive

--- local-server.sh
+++ local-server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+LOCAL_CONFIG="_local_development.yml"
 # copy the current config
-cp _config.yml _local_development.yml
-
+cp _config.yml $LOCAL_CONFIG
 # find the line with the remote_theme setting, delete it
-sed -i '/remote_theme/d' _local_development.yml
+sed -i '/remote_theme/d' $LOCAL_CONFIG
 
 # minimal-mistakes-jekyll is the only place where the full theme name is used, replace it
 # yes I know this is bad, no, I don't care
-sed -i '/minimal-mistakes-jekyll"/c\theme                    : "minimal-mistakes-jekyll"' _local_development.yml
+sed -i '/minimal-mistakes-jekyll"/c\theme                    : "minimal-mistakes-jekyll"' $LOCAL_CONFIG
 
 # run the server
-bundle exec jekyll serve --config _local_development.yml
+bundle exec jekyll serve --config $LOCAL_CONFIG
